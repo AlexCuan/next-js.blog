d0aa0fd55d343a8ebb712cd1f8d90ee3b5e6259f
Alex <alex@alexcuan.com>
2023-05-09 13:30:12 +0200

    Fix

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Layout, {siteTitle} from './components/layout';
 import utilStyles from '../styles/utils.module.css';
 import {getSortedPostsData} from '../lib/posts';
 import Link from 'next/link';
-import Date from './components/date';
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
@@ -31,13 +30,12 @@ export default function Home({allPostsData}) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({id, date, title}) => (
+                    {allPostsData.map(({id, title}) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href={`/posts/${id}`}>{title}</Link>
                             <br/>
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date}/>
-                            </small>
+                            <br/>
+
                         </li>
                     ))}
                 </ul>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from '../components/layout';
 import {getAllPostIds, getPostData} from '../../lib/posts';
 import Head from "next/head";
-import Date from '../components/date';
+
 import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticPaths() {
@@ -29,9 +29,7 @@ export default function Post({ postData }) {
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
+
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
